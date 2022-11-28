d1aa2a9dc70fea4a164111a6ce7e9e55239b067e
Lorena Cueva <lorenacueva@gmail.com>
2022-11-28 15:06:23 -0800

    Adds Idea instance methods

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,15 @@ class Idea < ActiveRecord::Base
     belongs_to :topic
     belongs_to :user
     has_many :likes
+
+    def likes_count
+        self.likes.count
+    end
+
+    def liked_by
+        arr = []
+        self.likes.each {|i| arr << i.user.name}
+        arr
+    end
+
 end
