630065ee362ddc92d3e932a6c07c20060098ac0c
Lorena Cueva <lorenacueva@gmail.com>
2022-11-28 15:06:06 -0800

    Adds Topic instance methods

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,18 @@ class Topic < ActiveRecord::Base
     belongs_to :user
     has_many :ideas
     has_many :likes, through: :ideas
+
+    def ideas_count
+       self.ideas.count
+    end
+
+    def closed_on
+        if !open
+            edited_at
+        end 
+    end
+
+    def winner
+        Idea.find(winner_idea).body
+    end
 end
