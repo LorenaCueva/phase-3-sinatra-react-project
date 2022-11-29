fe27c70bac89a1d95b01ca8f3aa944a047b70764
Lorena Cueva <lorenacueva@gmail.com>
2022-11-28 16:36:29 -0800

    Topics CRUD paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,17 +17,17 @@ class ApplicationController < Sinatra::Base
 
   get "/topics" do
     topics = Topic.all.order(created_at: :desc)
-    topics.to_json(only: [:id , :title, :user_id, :created_at, :edited_at], include: {ideas: {:methods => [:likes_count, :liked_by]}}, :methods => [:ideas_count])
+    topics.to_json(only: [:id , :title, :user_id, :created_at, :updated_at], include: {ideas: {:methods => [:likes_count, :liked_by]}}, :methods => [:ideas_count])
   end
 
   get "/topics/open" do
     topics = Topic.where("open = ?", true).order(created_at: :desc)
-    topics.to_json(only: [:id , :title, :user_id, :created_at, :edited_at], include: {ideas: {:methods => [:likes_count, :liked_by]}}, :methods => [:ideas_count])
+    topics.to_json(only: [:id , :title, :user_id, :created_at, :updated_at], include: {ideas: {only: [:id, :body, :user_id, :created_at, :updated_at], :methods => [:likes_count, :liked_by]}}, :methods => [:ideas_count])
   end
 
   get "/topics/open/:user_id" do
     topics = Topic.where("user_id = ? AND open = ?",  params[:user_id], true)
-    topics.to_json(only: [:id , :title, :created_at, :edited_at], include: {ideas: {:methods => [:likes_count, :liked_by]}}, :methods => [:ideas_count])
+    topics.to_json(only: [:id , :title, :created_at, :updated_at], include: {ideas: {only: [:id, :body, :created_at, :updated_at], :methods => [:likes_count, :liked_by]}}, :methods => [:ideas_count])
   end
 
   get "/topics/closed" do
@@ -36,16 +36,60 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/topics/closed/:user_id" do
-    topics = Topic.where("user_id = ? AND open = ?", params[:user_id], false).order(edited_at: :desc)
+    topics = Topic.where("user_id = ? AND open = ?", params[:user_id], false).order(updated_at: :desc)
     topics.to_json(only: [:id , :title, :created_at], :methods => [:closed_on, :ideas_count, :winner])
   end
 
   get "/topic/:id" do
     topics = Topic.where("id = ?", params[:id])
-    topics.to_json(only: [:id, :title, :user_id, :created_at, :edited_at, :winner_idea], include: {ideas: {:methods => [:likes_count, :liked_by]}}, :methods => [:ideas_count])
+    topics.to_json(only: [:id, :title, :user_id, :created_at, :updated_at, :winner_idea], include: {ideas: {:methods => [:likes_count, :liked_by]}}, :methods => [:ideas_count])
   end
 
-  
+  patch "/topic/:id" do
+    topic = Topic.find(params[:id])
+    topic.update(
+      title: params[:title],
+      open: params[:open],
+      winner_idea: params[:winner_idea]
+    )
+    topic.to_json
+  end
+
+  post "/topics" do
+    topic = Topic.create(
+      title: params[:title],
+      user_id: params[:user_id],
+      open: true
+    )
+    topic.to_json
+  end
+
+  delete "/topic/:id" do
+    topic = Topic.find(params[:id])
+    topic.destroy
+    topic.to_json
+  end
+
+  get "/topic/:id/ideas" do
+    ideas = Idea.where("topic_id = ?", params[:id])
+    ideas.to_json(only: [:id, :body, :created_at, :updated_at], :methods => [:likes_count, :liked_by])
+  end
+
+  delete "/idea/:id" do
+    idea = Idea.find(params[:id])
+    idea.destroy
+    idea.to_json
+  end
+
+  post "/topic/:id/ideas" do
+    idea = idea.create(
+      topic_id: params[:id],
+      body: params[:body],
+      user_id: params[user_id],
+    )
+    idea.to_json
+  end
+
 
 
 end
