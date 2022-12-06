3de25c5d11d3ef642cc905e198622fdfd586ff0a
Lorena Cueva <lorenacueva@gmail.com>
2022-12-05 17:14:11 -0800

    Idea class and controller changes

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -10,23 +10,28 @@ class IdeasController < Sinatra::Base
         format_to_json(res)
     end
     
-    get "/ideas/topic/:id" do
-        ideas = Idea.where("topic_id = ?", params[:id]).order(created_at: :desc)
+    get "/ideas/:topic_id" do
+        ideas = Idea.where("topic_id = ?", params[:topic_id]).order(created_at: :desc)
         format_to_json(ideas)
     end
     
-    post "/ideas/topic/:id" do
+    post "/ideas/:topic_id" do
         idea = Idea.create(
-          topic_id: params[:id],
+          topic_id: params[:topic_id],
           body: params[:body],
           user_id: params[:user_id],
         )
-        idea.to_json
+        Like.create(
+            idea_id: idea.id,
+            user_id: idea.user_id
+        )
+        newIdea = Idea.find(idea.id)
+        format_to_json(newIdea)
     end
     
     get "/idea/:id" do
         idea = Idea.find(params[:id])
-        Idea.to_json(idea)
+        format_to_json(idea)
     end
     
     patch "/idea/:id" do
@@ -41,10 +46,27 @@ class IdeasController < Sinatra::Base
         idea.to_json
     end
 
+    delete "/idea/:id/:user_id/unlike" do
+        like = Like.where("idea_id = ? AND user_id = ?", params[:id], params[:user_id])
+        like[0].destroy
+        idea = Idea.find(params[:id]);
+        idea.to_json(:methods => [:likes_count, :liked_by])
+    end
+
+    post "/idea/:id/:user_id/like" do
+        like = Like.create(
+            user_id: params[:user_id],
+            idea_id: params[:id]
+        )
+        idea = Idea.find(params[:id]);
+        idea.to_json(:methods => [:likes_count, :liked_by])
+    end
+
     private
 
     def format_to_json(ideas)
-        ideas.to_json(except: [:user_id], :methods => [:author, :topic_title, :likes_count, :liked_by])
+        ideas.to_json(:methods => [:author, :topic_title, :likes_count, :liked_by])
     end
+
 
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,15 +3,25 @@ class Idea < ActiveRecord::Base
     belongs_to :user
     has_many :likes, :dependent => :destroy
 
+    # const liked_by_current_user? = false;
+
     def likes_count
         self.likes.count
     end
 
     def liked_by
-        arr = []
-        self.likes.each {|i| arr << i.user.name}
-        arr
+        res = []
+        self.likes.each {|i| res << i.user_id}
+        res
     end
+
+    # def liked_by_user?(user)
+    #     liked_by_current_user? = liked_by.include?(user)
+    # end
+
+    # def li?
+    #     liked_by_current_user
+    # end
 
     def author
         self.user.name
