a8dd99fa64b324af4b03d5b0630b50ca2924b8cf
Lorena Cueva <lorenacueva@gmail.com>
2022-12-12 10:31:40 -0800

    Updated Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,84 @@
+require 'faker'
+
 puts "🌱 Seeding db..."
 
 puts "Creating users..."
 
-u1 = User.create(name: "Aki", email: "aki@aki.com")
-u2 = User.create(name: "Lorena", email: "lorenacueva@gmail.com")
-u3 = User.create(name: "Crocky", email: "crocky@aki.com")
-u4 = User.create(name: "Panda", email: "panda@panda.com")
+
+u1 = User.create(name: "Aki", password: "aki")
+u2 = User.create(name: "Lore", password: "lore")
+u3 = User.create(name: "Crock", password: "crock")
+u4 = User.create(name: "Panda", password: "panda")
+
+20.times do |i|
+    User.create(name: Faker::Name.unique.name, password: Faker::Internet.unique.password)
+end
+
 
 puts "Creating topics..."
 
-t1 = Topic.create(title: "Places to hide bone", user: u1, open: true)
-t2 = Topic.create(title: "Restaurant to try:", user: u2, open: false)
-t3 = Topic.create(title: "Places to hide from Dog:", user: u3, open: true)
-t4 = Topic.create(title: "Ways to show more grumpusness:", user: u1, open: true)
-t5 = Topic.create(title: "What to do this weekend", user: u2, open: false, updated_at: Time.now.getutc)
+20.times do |i|
+    Topic.create(title: "Topic# #{i}", user_id: Faker::Number.between(from: 1, to: 24), open: true)
+end
+
+20.times do |i|
+    Topic.create(title: "Topic# #{i + 20}", user_id: Faker::Number.between(from: 1, to: 4), open: true)
+end
+
+20.times do |i|
+    Topic.create(title: "Topic# #{i + 40}", user_id: Faker::Number.between(from: 1, to: 24), open: false, winner_idea: Faker::Number.between(from: 1, to: 100) )
+end
+
+t1 = Topic.create(title: "Places to hide bone", user_id: 1, open: true)
+
+ideas = ["Under the pillow", "In the corner of the desk", "In the mole's hole", "Inside the crocky", "Don't hide it, eat it all!"]
+
+ideas.each do |i|
+    Idea.create(body: i, user_id: Faker::Number.between(from: 1, to: 4) , topic: t1)
+end
+
+40.times do |i|
+    Like.create(idea_id: Faker::Number.between(from: 1, to: 5), user_id: Faker::Number.between(from: 1, to: 24) )
+end
+
+t1 = Topic.create(title: "What to do on Christmas-Eve", user_id: 2, open: true)
+
+ideas = ["Go see Moulin Rouge", "Dinner at a Restaurant", "Quiet evening at home", "Dinner with Friends"]
+
+ideas.each do |i|
+    Idea.create(body: i, user_id: Faker::Number.between(from: 1, to: 4) , topic: t1)
+end
+
+40.times do |i|
+    Like.create(idea_id: Faker::Number.between(from: 6, to: 10), user_id: Faker::Number.between(from: 1, to: 24) )
+end
+
 
 puts "Creating ideas..."
 
-i1 = Idea.create(body: "Under the pillow", topic: t1, user: u1)
-i2 = Idea.create(body: "Under the chair", topic: t1, user:u2)
-i3 = Idea.create(body: "Ciudad in Georgetown", topic: t2 , user: u4)
-i4 = Idea.create(body: "Close to the washing machine", topic: t3 , user: u4)
-i5 = Idea.create(body: "Growl at any sound", topic: t4, user: u1)
-i6 = Idea.create(body: "Go fishing", topic: t5, user: u4)
-i7 = Idea.create(body: "Watch movies", topic: t5, user: u1)
+100.times do |x|
+    Idea.create(body: "Idea# #{x}", user_id: Faker::Number.between(from: 1, to: 4), topic_id: Faker::Number.between(from: 1, to: 40))
+end
 
-t5.winner_idea = i6.id
-t5.save
-t2.winner_idea = i3.id
-t2.save
+100.times do |x|
+    Idea.create(body: "Idea# #{x}", user_id: Faker::Number.between(from: 1, to: 24), topic_id: Faker::Number.between(from: 1, to: 40))
+end
 
-l1 = Like.create(idea: i6, user: u2)
-l2 = Like.create(idea: i6, user: u3)
-l3 = Like.create(idea: i1, user: u1)
-l4 = Like.create(idea: i1, user: u2)
 
+puts "Creating likes..."
+
+
+100.times do |i|
+    Like.create(idea_id: Faker::Number.between(from: 40, to: 60), user_id: Faker::Number.between(from: 1, to: 4) )
+end
+
+100.times do |i|
+    Like.create(idea_id: Faker::Number.between(from: 1, to: 100), user_id: Faker::Number.between(from: 1, to: 4) )
+end
+
+100.times do |i|
+    Like.create(idea_id: Faker::Number.between(from: 100, to: 200), user_id: Faker::Number.between(from: 1, to: 24) )
+end
 
 # Seed your database here
 
